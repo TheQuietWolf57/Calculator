30478d19c1fae3f7ef098c87c231e7d2cc8cd481
TheQuietWolf57 <nathaniel.goss465@gmail.com>
2025-02-07 18:18:58 -0500

    Improve GUI and create working functions for operators

--- script.js
+++ script.js
@@ -9,10 +9,20 @@ const ans = document.createElement("div");
 ans.classList.add("block");
 container.appendChild(ans);
 
+//Define top operator flexbox to house AC, (, and ) in
+const topOperator = document.createElement("div");
+topOperator.classList.add("top");
+container.appendChild(topOperator); 
+
 //Define numpad to house digits in
 const numpad = document.createElement("div");
 numpad.classList.add("numpad");
 container.appendChild(numpad);
+
+//Define side operator flexbox to house + - / and X
+const sideOperator = document.createElement("div");
+sideOperator.classList.add("side");
+container.appendChild(sideOperator);
 
 //Initialize global variables
 let num1 = 0;
@@ -20,7 +30,7 @@ let num2 = 0;
 let op = "";
 
 // Define each button with the appropriate symbol
-const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const numbers = ["7", "8", "9", "4", "5", "6", "1", "2", "3", ".", "0", "="];
 for(symbol in numbers) {
     num = document.createElement("button");
     num.textContent = numbers[symbol];
@@ -28,19 +38,30 @@ for(symbol in numbers) {
     numpad.appendChild(num);
 }
 
-const operators = [".", "AC", "+", "-", "X", "/", "%", "(", ")", "+"]
-for(operator in operators) {
+
+const side = ["/","x","-","+"];
+for(operator in side) {
     symbol = document.createElement("button");
-    symbol.textContent = operators[operator];
+    symbol.textContent = side[operator];
     symbol.classList.add("operator");
     symbol.addEventListener("click", () => {
         
     });
-    container.appendChild(symbol);
+    sideOperator.appendChild(symbol);
 }
 
-const misc = ["AC", "(", ")", ] 
+const topOp = ["AC", "(", ")", "%"];
+for(operator in topOp) {
+    symbol = document.createElement("button");
+    symbol.textContent = topOp[operator];
+    symbol.classList.add("operator");
+    symbol.addEventListener("click", () => {
+        
+    });
+    topOperator.appendChild(symbol);
+}
 
+ans.append(num1);
 //Create functions for each operator
 function add(x, y) {
     const sum = x + y;
@@ -61,20 +82,6 @@ function mult(x, y) {
     const prod = x * y;
     return prod;
 }
-
-
-console.log(mult (5, 3));
-console.log(add(5, 3));
-console.log(sub(5, 3));
-console.log(div(5, 3));
-console.log(mult(5, -3));
-console.log(add(5, -3));
-console.log(sub(5, -3));
-console.log(div(5, -3));
-console.log(mult(5, .3));
-console.log(add(5, .3));
-console.log(sub(5, .3));
-console.log(div(5, .3));
 
 //function perc();
 
